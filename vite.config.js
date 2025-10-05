a33efc9aa993e509a976791e448e8b59e96738f7
aticem <atilay376@gmail.com>
2025-10-05 18:28:25 +0100

    last

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+// Copy
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { host: true, port: 5173 },
-  preview: { host: true, port: 5173 }
+  server: { host: true, port: 5174, strictPort: true },
+  preview: { host: true, port: 5174 }
 })
